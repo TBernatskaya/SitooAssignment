881512ceff9e134790394d52b8129a10fcfc7b69
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2020-01-27 14:01:43 +0100

    Revert "Added pod OHHTTPStubs/Swift"
    This reverts commit 2e1d17c823d8ed500a874eca084fdbfe6ee47066.

--- SitooAssignment/Pods/Target Support Files/Pods-SitooAssignment/Pods-SitooAssignment-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SitooAssignment : NSObject
-@end
-@implementation PodsDummy_Pods_SitooAssignment
-@end
--- SitooAssignment/Pods/Target Support Files/Pods-SitooAssignmentTests/Pods-SitooAssignmentTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SitooAssignmentTests : NSObject
-@end
-@implementation PodsDummy_Pods_SitooAssignmentTests
-@end
